fdf3fb5ffb61723ec0a8bd2ee3286880d538d30a
niekados <tiny.post@yahoo.com>
2021-09-08 19:35:47 +0000

    Update task into the database

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def add_task():
         task = {
             "category_name": request.form.get("category_name"),
             "task_name": request.form.get("task_name"),
-            "task_description": request.form.getlist("task_description"),
+            "task_description": request.form.get("task_description"),
             "is_urgent": is_urgent,
             "due_date": request.form.get("due_date"),
             "created_by": session["user"]
@@ -121,6 +121,21 @@ def add_task():
 
 @app.route("/edit_task/<task_id>", methods=["GET", "POST"])
 def edit_task(task_id):
+    if request.method == "POST":
+        # "task_description": request.from.getlist("task_description") - getlist- is good to get multiple elements if you need to get a list like array of items
+        is_urgent = "on" if request.form.get("is_urgent") else "off"
+        submit = {
+            "category_name": request.form.get("category_name"),
+            "task_name": request.form.get("task_name"),
+            "task_description": request.form.get("task_description"),
+            "is_urgent": is_urgent,
+            "due_date": request.form.get("due_date"),
+            "created_by": session["user"]
+        }
+        mongo.db.tasks.update({"_id": ObjectId(task_id)}, submit)
+        flash("Task Successfully updated")
+        
+
     task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
 
     categories = mongo.db.categories.find().sort("category_name", 1)
